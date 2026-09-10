acac69d9556a26ed5e43f1e49276f9d465fcd261
bob <augustuschan@gmail.com>
2026-09-10 16:34:08 +0800

    chore: initial commit

---
+++ src/test/java/gustus/roadmap/tasktracker/TasktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package gustus.roadmap.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
