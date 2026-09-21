a2f41ed7cc792ef04c210bb0e8b426357b1419f9
bob <augustuschan@gmail.com>
2026-09-21 16:14:49 +0800

    fix: extend JpaRepository instead of CrudRepository

--- src/main/java/gustus/roadmap/tasktracker/Repository/AppTaskRepository.java
+++ src/main/java/gustus/roadmap/tasktracker/Repository/AppTaskRepository.java
@@ -2,12 +2,12 @@ package gustus.roadmap.tasktracker.Repository;
 
 import gustus.roadmap.tasktracker.Entity.AppTask;
 import gustus.roadmap.tasktracker.Enum.AppTaskStatus;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface AppTaskRepository extends CrudRepository<AppTask, Long> {
+public interface AppTaskRepository extends JpaRepository<AppTask, Long> {
     List<AppTask> findAllByStatusContaining(AppTaskStatus status);
 }
